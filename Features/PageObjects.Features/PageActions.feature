dc82298f615861bd2de0b242f9a8e1b1b56f58a7
unknown <Leon@Leon-PC.(none)>
2012-05-22 20:37:25 +0100

    added packages and put initial steps in place

---
+++ Features/PageObjects.Features/PageActions.feature
@@ -0,0 +1,12 @@
+﻿Feature: Page Actions
+	In order to interact with a web page
+	As a tester
+	I want to a page object that encapsulates interactions
+
+Background:
+	Given I have a browser
+
+Scenario: Getting a page object
+	Given there is a static example page
+	When I request the page
+	Then I should receive a page object with the url of the static example page
---
+++ Features/PageObjects.Features/step_definitions/PageActions_steps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Coypu;
+using TechTalk.SpecFlow;
+
+namespace PageObjects.Features.step_definitions
+{
+    [Binding]
+    public class PageActions_steps
+    {
+        private BrowserSession _browser;
+        private string _examplePageUrl;
+        private object _examplePage;
+
+        [Given(@"I have a browser")]
+        public void CreateBrowser()
+        {
+            _browser = new BrowserSession();
+        }
+
+        [Given(@"there is a static example page")]
+        public void GivenThereIsAStaticExamplePage()
+        {
+            _examplePageUrl = "@static_page.html";
+        }
+
+        [When(@"I request the page")]
+        public void WhenIRequestThePage()
+        {
+            on_page<Example_Page>(page => _examplePage = page);
+        }
+
+        [Then(@"I should receive a page object with the url of the static example page")]
+        public void ThenIShouldReceiveAPageObjectWithTheUrlOfTheStaticExamplePage()
+        {
+            _examplePage.Url.ShouldBe(_examplePageUrl);
+        }
+
+    }
+}
